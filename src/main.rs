37bbf0c347aca48e59a3d4a85501bc12e87b0464
Kris Warner <kdwarn@protonmail.com>
2022-11-15 15:26:24 -0500

    Collect errors into a file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io::Write;
 use std::path::{Path, PathBuf};
 
 use csv::Writer;
@@ -47,16 +48,16 @@ impl Connection {
         connection.provider = Some(provider);
 
         if connection.data_source.is_none() || connection.user_id.is_none() {
-            return Err("Notice: no data source or user id in string".to_string());
+            return Err("No data source or user id in string".to_string());
         }
         Ok(connection)
     }
 }
 
-fn extract(f: &Path) -> Vec<Connection> {
-    // let config = ParserConfig::new().trim_whitespace(true);
-
+/// Extract connection string from xml, return tuple of successes and errors
+fn extract(f: &Path) -> (Vec<Connection>, Vec<String>) {
     let mut connections = vec![];
+    let mut errors = vec![];
 
     let content = fs::read_to_string(f).unwrap();
     for line in content.lines() {
@@ -72,19 +73,20 @@ fn extract(f: &Path) -> Vec<Connection> {
                         "Unrecognized provider format".to_string()
                     };
 
-                    if let Ok(v) = Connection::new(
+                    match Connection::new(
                         f.to_string_lossy().to_string(),
                         v.connection_string.clone(),
                         provider,
                     ) {
-                        connections.push(v)
+                        Ok(v) => connections.push(v),
+                        Err(e) => errors.push(format!("{:?}: {}", f, e)),
                     }
                 }
-                Err(e) => println!("{e}"),
+                Err(e) => errors.push(format!("{:?}: {}", f, e)),
             }
         }
     }
-    connections
+    (connections, errors)
 }
 
 // Get files matching extensions we want
@@ -109,27 +111,40 @@ fn main() -> std::io::Result<()> {
     let dir = Path::new(DIR);
     let files = vec![];
     let mut connections = vec![];
+    let mut errors = vec![];
 
     if dir.is_dir() {
         if let Ok(v) = traverse(dir.to_path_buf(), files) {
             for file in v {
-                connections.extend(extract(&file));
+                let (c, e) = extract(&file);
+                connections.extend(c);
+                errors.extend(e);
             }
         }
     }
 
-    // write to CSV
-    let mut wtr = Writer::from_path("output.csv")?;
-    wtr.write_record(["path", "data source", "user id", "provider"])?;
-    for cs in connections {
-        wtr.write_record(&[
-            cs.path.unwrap(),
-            cs.data_source.unwrap(),
-            cs.user_id.unwrap(),
-            cs.provider.unwrap(),
-        ])?;
+    // write to connections to CSV file
+    if !connections.is_empty() {
+        let mut wtr = Writer::from_path("output.csv")?;
+        wtr.write_record(["path", "data source", "user id", "provider"])?;
+        for cs in connections {
+            wtr.write_record(&[
+                cs.path.unwrap(),
+                cs.data_source.unwrap(),
+                cs.user_id.unwrap(),
+                cs.provider.unwrap(),
+            ])?;
+        }
+        wtr.flush()?;
     }
-    wtr.flush()?;
+
+    // write errors to text file
+    if !errors.is_empty() {
+        let mut error_file = fs::File::create("errors.txt")?;
+        for error in errors {
+            writeln!(error_file, "{}", error)?;
+        }
+    }
 
     Ok(())
 }
